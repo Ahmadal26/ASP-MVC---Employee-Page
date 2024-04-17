42736bc2fffade861dc55b291b47041774ec85fd
KFH <KFH@LAPTOP-TRN-145>
2024-04-17 14:49:34 +0300

    Add project files.

---
+++ WebApplication2/Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class EmployeeController : Controller
+
+    {
+        static List<Employee> employees = new List<Employee>
+{
+
+    new Employee {EmployeeId = "833217",FirstName= "Ahmad",LastName = "Musallam",Email = "833217@kfh.com",Department  = "IT"},
+    new Employee {EmployeeId = "833216",FirstName= "Mubark",LastName = "Oraifan",Email = "833216@kfh.com",Department  = "HR"},
+    new Employee {EmployeeId = "831315",FirstName= "Abdullah",LastName = "Binessa",Email = "831315@kfh.com",Department  = "IT"},
+};
+
+        public IActionResult Index()
+        {
+            return View(employees);
+        }
+
+        [HttpGet]
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Submit(IFormCollection from)
+        {
+            var firstName = from["FirstName"];
+            var lastName = from["LastName"];
+            var email = from["Email"];
+            var department = from["Department"];
+            var employeeId = from["EmployeeId"];
+
+            employees.Add(new Employee()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Email = email,
+                Department = department,
+                EmployeeId = employeeId,
+
+            });
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(string employeeId)
+        {
+            var employee = employees.Find(c => c.EmployeeId == employeeId);
+            if (employee == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+                return View(employee);
+        }
+
+    }
+}
---
+++ WebApplication2/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using WebApplication2.Models;
+
+namespace WebApplication2.Models
+{
+    public class Employee
+    {
+        public string EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+      //  public DateTime DateOfBirth { get; set; }
+       // public string FavoriteColor { get; set; }
+
+    }
+
+}
