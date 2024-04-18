0615e7a818b446d887d82b5b666d977c38a8f851
KFH <KFH@LAPTOP-TRN-145>
2024-04-18 15:18:21 +0300

    Done all

--- WebApplication2/Models/Employee.cs
+++ WebApplication2/Models/Employee.cs
@@ -1,14 +1,23 @@
-﻿using WebApplication2.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using WebApplication2.Models;
 
 namespace WebApplication2.Models
 {
     public class Employee
     {
+        [Required]
         public string EmployeeId { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Department { get; set; }
+
+
+
       //  public DateTime DateOfBirth { get; set; }
        // public string FavoriteColor { get; set; }
 
